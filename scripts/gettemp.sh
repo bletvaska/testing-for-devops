6335fa8aee26071b2f8fc6bc58067f3e8d292600
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-08-25 09:15:44 +0200

    added ultra mega supa script for getting the temperature

---
+++ scripts/gettemp.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+ 
+set -o errexit  # stop when error occurs
+set -o pipefail # if not, expressions like `error here | true`
+                # will always succeed
+set -o nounset  # detects uninitialised variables
+[[ "${DEBUG:-}" ]] && set -o xtrace   # prints every expression
+                                       # before executing it (debugging)
+ 
+readonly APPID="08f5d8fd385c443eeff6608c643e0bc5"
+# source .env
+ 
+function usage(){
+    echo "Usage: ./gettemp.sh [city,countrycode]"
+    echo "Created by (c)2022 peto, mirek, michal"
+}
+
+function get_temp(){
+    local location="$1"
+ 
+    [[ -z "${location}" ]] && {
+        usage
+        echo "Error: No city name given, which is required." >&2
+        exit 1
+    }
+ 
+    temp=$(jq .main.temp <(http "http://api.openweathermap.org/data/2.5/weather?units=metric&q=${location}&appid=${APPID}"))
+
+    echo "${temp}°C"
+}
+ 
+function main(){
+    local location="$1"
+    get_temp "${location}"
+}
+ 
+# call the func only if the script is executed directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main "$@"
+fi
+
