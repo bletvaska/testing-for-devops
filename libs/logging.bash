500a1a665ff2a86ce0b38b873e9ea912be99e639
Ubuntu <ubuntu@ip-172-31-39-130.eu-central-1.compute.internal>
2022-11-15 18:21:05 +0100

    created simple logging module

--- libs/logging.bash
+++ libs/logging.bash
@@ -1,14 +1,65 @@
-function _log(){
-    local level="${1}"
-    local message="${2}"
-    local ts=$(date "+%F %T")
+declare -ar _LEVEL_NAMES='(
+    [10]="DEBUG" 
+    [20]="INFO" 
+    [30]="WARNING" 
+    [40]="ERROR" 
+    [50]="CRITICAL"
+)'
 
-    printf "%s %s: %s\n" "${ts}" "${level}" "${message}"
+
+function _log(){
+    local level="${1:?Missing log level.}"
+    local message="${2:?Missing log message.}"
+    local ts=$(date "+%F %T")
+    local stream=1
+
+    # print all messages with log level >= 40 to stderr
+    if [[ $level -ge 40 ]]; then
+        stream=2
+    fi
+
+    # print log message
+    printf "%s %s: %s\n" \
+           "${ts}" "${_LEVEL_NAMES[${level}]}" "${message}" \
+           >&${stream}
 }
 
 
-function error(){
-    local message="${1}"
+# print DEBUG log message
+function debug(){
+    local message="${1:?Missing message.}"
 
-    _log "ERROR" "${message}" 
+    _log 10 "${message}" 
+}
+
+
+# print INFO log message
+function info(){
+    local message="${1:?Missing message.}"
+
+    _log 20 "${message}" 
+}
+
+
+# print WARNING log message
+function warning(){
+    local message="${1:?Missing message.}"
+
+    _log 30 "${message}" 
+}
+
+
+# print ERROR log message
+function error(){
+    local message="${1:?Missing message.}"
+
+    _log 40 "${message}" 
+}
+
+
+# print CRITICAL log message
+function critical(){
+    local message="${1:?Missing message.}"
+
+    _log 50 "${message}" 
 }
